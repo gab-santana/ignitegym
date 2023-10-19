5c999a958790adb9bc8c8f0ea5ce346b1a9b20dd
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-10-19 15:24:32 -0300

    add Fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {fontsLoaded ? <Text>Hello world</Text> : <View />}
     </View>
   );
 }
