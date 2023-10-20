08833247dd123b8678465b7dca34dcbf6425f2f6
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-10-20 10:14:42 -0300

    add screen SignIn and SignUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,7 +4,7 @@ import LogoSvg from '@assets/logo.svg'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
-export function SignIn() {
+export function SignUp() {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
       <VStack flex={1} bg={'gray.700'} px={10}>
@@ -30,8 +30,12 @@ export function SignIn() {
             fontFamily={'heading'}
             mb={6}
           >
-            Acesse sua conta
+            Crie sua conta
           </Heading>
+
+          <Input
+            placeholder='Nome'
+          />
 
           <Input
             placeholder='E-mail'
@@ -43,22 +47,19 @@ export function SignIn() {
             secureTextEntry
           />
 
-          <Button title='Acessar' />
+          <Button title='Criar e acessar' />
 
         </Center>
 
-        <Center mt={24}>
-          <Text
-            color={'gray.100'}
-            fontSize={'sm'}
-            mb={3}
-            fontFamily={'body'}
-          >
-            Ainda não tem acesso?
-          </Text>
 
-          <Button title='Criar conta' variant={'outline'} />
-        </Center>
+
+
+        <Button
+          title='Voltar para o login'
+          variant={'outline'}
+          mt={24}
+        />
+
       </VStack>
     </ScrollView>
   )
