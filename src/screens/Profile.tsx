c950a05c9cc69f2e8efb1824fa627a6f431567e1
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-10-27 01:09:30 -0300

    add function change user photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,15 +2,57 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from "native-base";
 import { useState } from 'react'
-import { TouchableOpacity } from "react-native";
+import {TouchableOpacity } from "react-native";
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system'
+import { FileInfo } from "expo-file-system";
 
 const PHOTO_SIZE = 33
 
 export function Profile() {
-
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState('https://github.com/gab-santana.png')
+
+  const toast = useToast()
+
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true)
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+        selectionLimit: 1
+      })
+
+      if (photoSelected.canceled) {
+        return
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri) as FileInfo
+
+        if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+          return toast.show({
+            title: "Essa imagem é muito grande, escolha uma de até 5MB.",
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -26,12 +68,14 @@ export function Profile() {
             endColor={'gray.400'}
           /> :
             <UserPhoto
-              source={{ uri: 'https://github.com/gab-santana.png' }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuario"
               size={PHOTO_SIZE}
             />}
 
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={handleUserPhotoSelect}
+          >
             <Text
               color={'green.500'}
               fontWeight={'bold'}
